ffb20a5defb9e2d7a082be113e755c45514bdfea
atomicfruitcake <sam@wirewax.com>
2022-11-29 16:19:24 +0000

    Fix failing tests now that seeding is working

--- app/src/test/kotlin/kotsprings/services/UserServiceTest.kt
+++ app/src/test/kotlin/kotsprings/services/UserServiceTest.kt
@@ -16,10 +16,8 @@ internal class UserServiceTest {
     @Autowired
     lateinit var userService: UserService
 
-//    var testUserId1: Long = 0
-
-    private val testUser1 = UserModel(
-        id = 1,
+    private val testUser = UserModel(
+        id = 11,
         userName = "aliceFoo1",
         firstName = "Alice",
         lastName = "Foo",
@@ -30,35 +28,41 @@ internal class UserServiceTest {
     )
 
     private fun cleanup() {
-        userService.deleteUser(testUser1.id)
+        userService.deleteUser(testUser.id)
     }
 
     @Test
     fun `test create and fetch of a user`() {
-        val user1 = userService.getUser(userService.createUser(testUser1).id)
-        assertEquals(user1, testUser1)
-        userService.deleteUser(testUser1.id)
+        val user = userService.getUser(userService.createUser(testUser).id)
+        assertEquals(user, testUser)
+    }
+
+    @Test
+    fun `test fetch a user`() {
+        val targetUserId = 1L
+        val user1 = userService.getUser(targetUserId)
+        assertEquals(user1?.id ?: -1, targetUserId)
     }
 
     @Test
     fun `test user can update email if email is valid`() {
         val newEmailAddress = "new@email.com"
-        val testUserId1 = userService.createUser(testUser1).id
-        userService.updateUserEmail(testUserId1, newEmailAddress)
-        val user1 = userService.getUser(testUserId1)
+        val targetUserId = 1L
+        userService.updateUserEmail(targetUserId, newEmailAddress)
+        val user1 = userService.getUser(targetUserId)
         assertEquals(user1?.emailAddress, newEmailAddress)
-        userService.deleteUser(testUser1.id)
+        userService.deleteUser(testUser.id)
     }
 
     @Test
     fun `test user cannot update email if email is invalid`() {
-        userService.deleteUser(testUser1.id)
-        val testUserId1 = userService.createUser(testUser1).id
+        userService.deleteUser(testUser.id)
+        val testUserId1 = userService.createUser(testUser).id
         val invalidEmailAddress = "NOT A VALID EMAIL"
         assertThrows<InvalidEmailException> {
             userService.updateUserEmail(testUserId1, invalidEmailAddress)
         }
-        userService.deleteUser(testUser1.id)
+        userService.deleteUser(testUser.id)
     }
     @BeforeEach
     fun beforeEach() {
